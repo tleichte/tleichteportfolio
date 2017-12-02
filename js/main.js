aa22b0d1ceda170537ef8e81735095dbd5ac162b
tleichte <tleichte@mtu.edu>
2017-12-01 19:23:37 -0500

    Made a placeholder site instead.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function main() {
-  $(".project_content").hide();
-  $(".project_button").on("click", function() {
-    $(this).next().slideToggle(400);
-    $(this).next().show();
-  })
-}
-$(document).ready(main);
